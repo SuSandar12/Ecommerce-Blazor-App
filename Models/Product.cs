7936bdcc2f365d641283f447b30e0156cfde7649
Suzy.Swe <Suzy.Swe@TWGTEA.com>
2023-12-10 23:40:01 +0800

    initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public string LongDescription { get; set; }
+}
